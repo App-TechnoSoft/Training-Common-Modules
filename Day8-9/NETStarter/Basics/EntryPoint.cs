b64b77d16753d009952f8ff5f1ad587505af732a
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-04-12 09:53:33 +0545

    Add rectangle class

---
+++ Day8-9/NETStarter/Basics/EntryPoint.cs
@@ -0,0 +1,41 @@
+﻿// Top level statements
+// Student bishnu = new();
+// Student laxman = new();
+// Student sandip = new();
+// Student rajan = new();
+// Student chahana = new();
+// Student bikendra = new();
+
+// bishnu.name = "Bishnu";
+// bishnu.address = "Ktm";
+// bishnu.roll = 23;
+// bishnu.dob = new DateTime(1990, 12, 12);
+
+// bishnu.PrintDetails();
+
+// Create class called "Animal"
+// Define at least 5 internal fields
+// Define a method to print all details of animal in descriptive format
+// In program entry point, create at least two objects of Animal
+// Initialize fields for each instance
+// Print details in concole
+
+
+var rec1 = new Rectangle(5.7, 3.4);
+var p = rec1.GetPerimeter();
+
+var rec2 = new Rectangle(123.23, 34.3);
+var a = rec2.GetArea();
+
+
+
+
+
+
+// Inheritance/Polymorphism -- OOP
+
+// Generics
+// LINQ
+// Async/Await
+// Attributes
+
